2ea1288de4c9221bf9d6455e942e3553e77f251f
Pedro Ferreira <pedro@thingston.com>
2022-12-20 22:08:46 +0000

    Improve OpenApi specification

--- src/Specification/OpenApi.php
+++ src/Specification/OpenApi.php
@@ -5,16 +5,28 @@ declare(strict_types=1);
 namespace Thingston\OpenApi\Specification;
 
 /**
+ * This is the root document object of the OpenAPI document.
+ *
+ * @link https://swagger.io/specification/#openapi-object
+ *
  * @property string $openapi
  * @property Info $info
+ * @property Paths $paths
+ * @property Servers|null $servers
+ * @property Components|null $components
+ * @property ExternalDocumentation|null $externalDocs
+ *
+ * @todo security
+ * @todo tags
  */
 final class OpenApi extends AbstractSpecification
 {
     public const OA_VERSION = '3.0.3';
 
-    public function __construct(Info $info, string $openapi = self::OA_VERSION)
+    public function __construct(Info $info, Paths $paths, string $openapi = self::OA_VERSION)
     {
         $this->properties['info'] = $info;
+        $this->properties['paths'] = $paths;
         $this->properties['openapi'] = $openapi;
     }
 
@@ -23,6 +35,16 @@ final class OpenApi extends AbstractSpecification
         return [
             'openapi' => 'string',
             'info' => Info::class,
+            'paths' => Paths::class,
+        ];
+    }
+
+    public function getOptionalProperties(): array
+    {
+        return [
+            'servers' => Servers::class,
+            'components' => Components::class,
+            'externalDocs' => ExternalDocumentation::class,
         ];
     }
 }
--- tests/Specification/OpenApiTest.php
+++ tests/Specification/OpenApiTest.php
@@ -4,23 +4,30 @@ declare(strict_types=1);
 
 namespace Thingston\OpenApi\Test\Specification;
 
+use Thingston\OpenApi\Specification\AbstractSpecification;
+use Thingston\OpenApi\Specification\Components;
+use Thingston\OpenApi\Specification\ExternalDocumentation;
 use Thingston\OpenApi\Specification\Info;
 use Thingston\OpenApi\Specification\OpenApi;
+use Thingston\OpenApi\Specification\Paths;
+use Thingston\OpenApi\Specification\Servers;
+use Thingston\OpenApi\Specification\Url;
 
 final class OpenApiTest extends AbstractSpecificationTest
 {
-    public function createMinimalSpecification(): \Thingston\OpenApi\Specification\AbstractSpecification
+    public function createMinimalSpecification(): AbstractSpecification
     {
-        $info = new Info('API title', '1.0');
-        $api = new OpenApi($info);
+        $api = new OpenApi(new Info('API title', '1.0'), new Paths());
 
         return $api;
     }
 
-    public function createFullSpecification(): \Thingston\OpenApi\Specification\AbstractSpecification
+    public function createFullSpecification(): AbstractSpecification
     {
-        $info = new Info('API title', '1.0');
-        $api = new OpenApi($info);
+        $api = new OpenApi(new Info('API title', '1.0'), new Paths());
+        $api->servers = new Servers();
+        $api->components = new Components();
+        $api->externalDocs = new ExternalDocumentation(new Url('http://example.org/docs'));
 
         return $api;
     }
